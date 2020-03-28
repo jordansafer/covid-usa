733f0c3fff422170b942b956a631c85e4d870f07
jordansafer <jordansafer2@gmail.com>
2020-03-28 14:19:04 -0400

    construct maps for counties and states

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
-var fs = require("fs")
+let fs = require("fs")
+let parse = require("csv-parse/lib/sync")
+let _ = require("lodash")
 
-var state_data = fs.readFileSync(`resources/us-states.csv`, 'utf8')
-var county_data = fs.readFileSync(`resources/us-counties.csv`, 'utf8')
+// date,state,fips,cases,deaths
+let state_csv = fs.readFileSync(`resources/us-states.csv`, 'utf8')
 
-console.log(state_data)
+// date,county,state,fips,cases,deaths
+let county_csv = fs.readFileSync(`resources/us-counties.csv`, 'utf8')
 
-module.exports = function (x) {
-      return x
+
+// parse state data
+let state_data = parse(state_csv)
+state_data.shift()
+
+state_info_by_date = {}
+
+_.forEach(state_data, row => {
+    let date = row[0]
+    let state = row[1]
+    let state_info = {
+        "cases": row[3],
+        "deaths": row[4]
+    }
+
+    let state_entries = state_info_by_date[date] || {}
+    state_entries[state] = state_info
+    state_info_by_date[date] = state_entries
+})
+
+// parse county data
+let county_data = parse(county_csv)
+county_data.shift()
+
+county_info_by_date = {}
+
+_.forEach(county_data, row => {
+    let date = row[0]
+    let county = row[1]
+    let state = row[2]
+    let county_info = {
+        "cases": row[4],
+        "deaths": row[5],
+        "state": state
+    }
+
+    // Update state map with county field
+    let state_entries = state_info_by_date[date] || {}
+    let state_info = state_entries[state] || {}
+    state_info[county] = county_info
+    state_entries[state] = state_info
+    state_info_by_date[date] = state_entries
+
+    // update county map
+    let county_entries = county_info_by_date[date] || {}
+    county_entries[state] = county_info
+    county_info_by_date[date] = county_entries
+})
+
+
+module.exports = {
+    // Dictionary with all state date
+    state: state_info_by_date,
+
+    // Dictionary with all county data
+    county: county_info_by_date
 };
 
 var myFn = function (x) {
--- test/test.js
+++ test/test.js
@@ -1,4 +1,18 @@
 var assert = require('assert')
 
-var data = require('../index.js')
+var lib = require('../index.js')
+
+let state_dict = lib.state
+
+let date = "2020-03-14"
+let state = "Massachusetts"
+
+console.log(state_dict[date][state])
+
+let county_dict = lib.county
+
+let county = "Middlesex"
+console.log(state_dict[date][state][county])
+console.log(county_dict[date][county])
+
 // assert.equals(a,b)
