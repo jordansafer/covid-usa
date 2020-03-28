057a7154dc8113e53a50e992279be5e7a8378532
jordansafer <jordansafer2@gmail.com>
2020-03-28 13:25:40 -0400

    import data

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var fs = require("fs")
+
+var state_data = fs.readFileSync(`resources/us-states.csv`, 'utf8')
+var county_data = fs.readFileSync(`resources/us-counties.csv`, 'utf8')
+
+console.log(state_data)
+
 module.exports = function (x) {
-      return x; 
+      return x
 };
 
 var myFn = function (x) {
-        return x;
+        return x
 };
