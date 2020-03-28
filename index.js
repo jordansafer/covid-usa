ecff7771d372a81284d89ca93a2cad64fae0e484
jordansafer <jordansafer2@gmail.com>
2020-03-28 14:35:30 -0400

    added eslint and made fixes

--- index.js
+++ index.js
@@ -1,71 +1,65 @@
-let fs = require("fs")
-let parse = require("csv-parse/lib/sync")
-let _ = require("lodash")
+const fs = require("fs")
+const parse = require("csv-parse/lib/sync")
+const _ = require("lodash")
 
 // date,state,fips,cases,deaths
-let state_csv = fs.readFileSync(`resources/us-states.csv`, 'utf8')
+const stateCsv = fs.readFileSync("resources/us-states.csv", "utf8")
 
 // date,county,state,fips,cases,deaths
-let county_csv = fs.readFileSync(`resources/us-counties.csv`, 'utf8')
-
+const countyCsv = fs.readFileSync("resources/us-counties.csv", "utf8")
 
 // parse state data
-let state_data = parse(state_csv)
-state_data.shift()
+const stateData = parse(stateCsv)
+stateData.shift()
 
-state_info_by_date = {}
+const stateInfoByDate = {}
 
-_.forEach(state_data, row => {
-    let date = row[0]
-    let state = row[1]
-    let state_info = {
-        "cases": row[3],
-        "deaths": row[4]
+_.forEach(stateData, row => {
+    const date = row[0]
+    const state = row[1]
+    const stateInfo = {
+        cases: row[3],
+        deaths: row[4]
     }
 
-    let state_entries = state_info_by_date[date] || {}
-    state_entries[state] = state_info
-    state_info_by_date[date] = state_entries
+    const stateEntries = stateInfoByDate[date] || {}
+    stateEntries[state] = stateInfo
+    stateInfoByDate[date] = stateEntries
 })
 
 // parse county data
-let county_data = parse(county_csv)
-county_data.shift()
+const countyData = parse(countyCsv)
+countyData.shift()
 
-county_info_by_date = {}
+const countyInfoByDate = {}
 
-_.forEach(county_data, row => {
-    let date = row[0]
-    let county = row[1]
-    let state = row[2]
-    let county_info = {
-        "cases": row[4],
-        "deaths": row[5],
-        "state": state
+_.forEach(countyData, row => {
+    const date = row[0]
+    const county = row[1]
+    const state = row[2]
+    const countyInfo = {
+        cases: row[4],
+        deaths: row[5],
+        state: state
     }
 
     // Update state map with county field
-    let state_entries = state_info_by_date[date] || {}
-    let state_info = state_entries[state] || {}
-    state_info[county] = county_info
-    state_entries[state] = state_info
-    state_info_by_date[date] = state_entries
+    const stateEntries = stateInfoByDate[date] || {}
+    const stateInfo = stateEntries[state] || {}
+    stateInfo[county] = countyInfo
+    stateEntries[state] = stateInfo
+    stateInfoByDate[date] = stateEntries
 
     // update county map
-    let county_entries = county_info_by_date[date] || {}
-    county_entries[state] = county_info
-    county_info_by_date[date] = county_entries
+    const countyEntries = countyInfoByDate[date] || {}
+    countyEntries[state] = countyInfo
+    countyInfoByDate[date] = countyEntries
 })
-
 
 module.exports = {
     // Dictionary with all state date
-    state: state_info_by_date,
+    state: stateInfoByDate,
 
     // Dictionary with all county data
-    county: county_info_by_date
-};
-
-var myFn = function (x) {
-        return x
-};
+    county: countyInfoByDate
+}
